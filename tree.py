4ca4eb9003f518fb4d98ebdd5f67a5a8ce5a3bbb
Siddharth <nairsiddu@gmail.com>
2025-03-23 17:59:58 +0000

    edits

--- tree.py
+++ tree.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 
 tree_colour = (0/255, 90/255, 45/255)  # tree colour
 
 
-def draw_tree(x, y, length, angle, branch_angle, shrink_factor, depth, thickness):
-    if depth == 0:
+def draw_tree(x, y, length, angle, branch_angle, shrink_factor, depth, thickness, decay_prob):
+    if depth == 0 or random.random() < decay_prob:
         return
 
     # end point of branches
@@ -17,11 +18,11 @@ def draw_tree(x, y, length, angle, branch_angle, shrink_factor, depth, thickness
     plt.plot([x, x_end], [y, y_end], color=tree_colour, lw=thickness)
 
     # branches
-    new_length = length * shrink_factor
+    new_length = length * shrink_factor 
     new_thickness = max(thickness * 0.7, 0.5)
 
-    draw_tree(x_end, y_end, new_length, angle - branch_angle, branch_angle, shrink_factor, depth - 1, new_thickness)
-    draw_tree(x_end, y_end, new_length, angle + branch_angle, branch_angle, shrink_factor, depth - 1, new_thickness)
+    draw_tree(x_end, y_end, new_length, angle - branch_angle, branch_angle, shrink_factor, depth - 1, new_thickness, decay_prob)
+    draw_tree(x_end, y_end, new_length, angle + branch_angle, branch_angle, shrink_factor, depth - 1, new_thickness, decay_prob)
 
 
 # parameters
@@ -29,12 +30,12 @@ init_x = 0
 init_y = -150
 branch_length = 100
 rotation = 90  # orientation
-branch_angle = 20  # angle
+branch_angle = 20 # angle
 branch_shrink = 0.7  
 depth = 8  # recursion
-trunk_thickness = 20
-background_colour = (230/255, 230/255, 0/255)  # background colour
-
+trunk_thickness = 16
+background_colour = (150/255, 100/255, 70/255)  # bg colour
+decay_probability = 0.09
 
 fig, ax = plt.subplots(figsize=(8, 8), facecolor=background_colour)
 ax.set_facecolor(background_colour)
@@ -43,11 +44,11 @@ plt.axis('off')
 plt.xlim(-170, 170)
 plt.ylim(-170, 170)
 
-draw_tree(init_x, init_y, branch_length, rotation, branch_angle, branch_shrink, depth, trunk_thickness)
+draw_tree(init_x, init_y, branch_length, rotation, branch_angle, branch_shrink, depth, trunk_thickness, decay_probability)
 
 # save as png
-filename = "fractal_tree.png"
-plt.savefig(filename, dpi=300, bbox_inches='tight', facecolor=background_colour)  
+filename = "fractal_tree_8D.png"
+plt.savefig(filename, dpi=600, bbox_inches='tight', facecolor=background_colour)  
 
 print(f"Image saved as '{filename}'")
 
