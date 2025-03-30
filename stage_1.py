b1e0417da8e80e5fac2854a1da51a4d0fac2d621
Siddharth <nairsiddu@gmail.com>
2025-03-30 11:23:13 +0100

    sandhya

--- stage_1.py
+++ stage_1.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 
 
-tree_colour = (0/255, 0/255, 0/255)  # tree colour
+tree_colour = (0/255, 90/255, 45/255)  # tree colour
 
 def draw_tree(x, y, length, angle, branch_angle, shrink_factor, depth, thickness, decay_prob):
     if depth == 0 or random.random() < decay_prob:
@@ -33,8 +33,8 @@ branch_angle = 20 # angle
 branch_shrink = 0.7  
 depth = 8  # recursion
 trunk_thickness = 16
-background_colour = (255/255, 255/255, 255/255)  # bg colour
-decay_probability = 0.00
+background_colour = (180/255, 220/255, 255/255)  # bg colour
+decay_probability = 0.08
 
 fig, ax = plt.subplots(figsize=(8, 8), facecolor=background_colour)
 ax.set_facecolor(background_colour)
@@ -52,4 +52,3 @@ plt.savefig(filename, dpi=600, bbox_inches='tight', facecolor=background_colour)
 print(f"Image saved as '{filename}'")
 
 plt.show()
-
